58ac58f466703563fe21f106ad497a92af86f74e
Francisco Quintero <francisco@repzio.com>
2018-10-22 17:05:44 -0400

    initial setup without having imported vuex to components

--- src/components/mainView.vue
+++ src/components/mainView.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div>Hello {{}}</div>
+        <div>Hello</div>
         <div>
             <router-link :to="{ path: 'ChildView'}">Click Here</router-link>
         </div>
@@ -9,14 +9,10 @@
 
 <script lang="ts">
 import Vue from "vue"
-import { mapState } from 'vuex'
 
 
 export default Vue.extend({
-    name: "MainView",
-    computed: mapState({
-        products: state => state.products.all
-    }),
+    name: "MainView"
 
 });
 </script>
